e140cff82ed299fbd4d215a0c3230dbfb235394a
antonyks <antonyksebastian@gmail.com>
2025-12-08 14:26:37 -0500

    fix(auth): fix user ban status issue

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,9 +1,9 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import { AuthCredentials } from "./auth.types";
-import { AuthUser } from '../user/user.model';
+import { AuthUser, UserStatus } from '../user/user.model';
 import { UserRepository } from '../user/user.repository';
-import { NotFoundError } from '../../errors'
+import { AuthenticationError, NotFoundError } from '../../errors'
 
 export const loginUser = async (data: AuthCredentials) => {
   const user:AuthUser|null = (await UserRepository.findByEmail(data.email,true)) as (AuthUser|null);
@@ -11,8 +11,11 @@ export const loginUser = async (data: AuthCredentials) => {
   if(!user)
     throw new NotFoundError("Account not found");
 
+  if(user.status==UserStatus.BANNED)
+    throw new AuthenticationError("Account is banned");
+
   const isValid = await bcrypt.compare(data.password, user.passwordHash);
-  if (!isValid) throw new Error("Invalid credentials");
+  if (!isValid) throw new AuthenticationError("Invalid credentials");
 
   const token = jwt.sign({ id: user.id, email:user.email,name:user.name, role:user.role, status:user.status }, process.env.JWT_SECRET || "secret", { expiresIn: "1d" });
 
